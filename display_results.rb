bb92c577dae2bd3fb9ff5f9c278b510743420385
vinnyk <ragdolleye@gmail.com>
2021-05-07 10:38:19 -0500

    Cleans up some code, adds comment to the movies_after_2017 print loop in display_results.rb

--- display_results.rb
+++ display_results.rb
@@ -3,7 +3,7 @@ require 'http'
 response = HTTP.get("http://localhost:3000/all_movies")
 movies = response.parse
 
-#clean-prints the movies
+# Clean-prints the movies
 movies.each do |movie|
   puts "The movie, #{movie["title"]}, was released in #{movie["year"]}"
   puts "PLOT:"
@@ -11,7 +11,7 @@ movies.each do |movie|
   puts
 end
 
-
+# Prints movies after 2017 to terminal using the movies_after_2017 method in the controller.
 response = HTTP.get("http://localhost:3000/movies_after_2017")
 movies_after_2017 = response.parse
 
