a238ba9265d8766c7b5d180812d1bffd481fff4d
vinnyk <ragdolleye@gmail.com>
2021-05-09 15:02:30 -0500

    Adds method to actors controller to accept a first and last name and return the first record as json data. Also adds associated route in routes.rb

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -4,5 +4,12 @@ class ActorsController < ApplicationController
     actor = Actor.find_by(id: 2)
     render json: {Actor_by_id_2: actor}
   end
+
+  def actor_by_full_name
+    first_name = params[:first_name].downcase.capitalize
+    last_name = params[:last_name].downcase.capitalize
+    actor = Actor.where("first_name = 'Benedict' AND last_name = 'Cumberbatch'")
+    render json: {message: actor}
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/actor_by_id", controller: "actors", action: "actor_by_id"
   get "individual_movie", controller: "movies", action: "individual_movie"
   get "all_movies", controller: "movies", action: "all_movies"
   get "movies_after_2017", controller: "movies", action: "movies_after_2017"
+
+  # Params homework - adds routes for query, segment, body parameters
+  # Query param:
+  get "actor_by_full_name" => "actors#actor_by_full_name"
 end
