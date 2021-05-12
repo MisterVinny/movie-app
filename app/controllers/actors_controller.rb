3abefd9e3d68560a86633fa621787e7dc7605985
vinnyk <ragdolleye@gmail.com>
2021-05-11 23:55:12 -0500

    Adds restful routes and restful controller actions for actors - index, create, show, update and destroy. Checked and all work as intended.

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,15 +1,48 @@
 class ActorsController < ApplicationController
 
-  def actor_by_id
-    actor = Actor.find_by(id: 2)
-    render json: {Actor_by_id_2: actor}
-  end
-
   def actor_by_full_name
     first_name = params[:first_name].downcase.capitalize
     last_name = params[:last_name].downcase.capitalize
     actor = Actor.where("first_name = '#{first_name}' AND last_name = '#{last_name}'")
     render json: actor.as_json
+  end
+
+
+  # Restful Actor actions
+  def index
+    actors = Actor.all
+    render json: actors.as_json
+  end
+  
+  def create
+    actor = Actor.new({
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      known_for: params[:known_for]
+    })
+    actor.save
+    render json: actor.as_json
+  end
+  
+  def show
+    actor = Actor.find(params[:id])
+    render json: actor.as_json
+  end
+  
+  def update
+    actor = Actor.find(params[:id])
+    actor.update({
+      first_name: params[:first_name] || actor.first_name,
+      last_name: params[:last_name] || actor.last_name,
+      known_for: params[:known_for] || actor.known_for
+      })
+      render json: actor.as_json
+    end
+    
+  def destroy
+    actor = Actor.find(params[:id])
+    actor.destroy
+    render json: {message: "Record for #{actor.first_name} #{actor.last_name} has been destroyed."}
   end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
-  get "/actor_by_id", controller: "actors", action: "actor_by_id"
-  get "individual_movie", controller: "movies", action: "individual_movie"
-  get "all_movies", controller: "movies", action: "all_movies"
-  get "movies_after_2017", controller: "movies", action: "movies_after_2017"
-
-  # Params homework - adds routes for query, segment, body parameters
-  # Query param: 
-  # This method expects a first_name and last_name. For example:
-  # ?first_name=benedict&last_name=Cumberbatch
   get "actor_by_full_name" => "actors#actor_by_full_name"
-  # Same as above but using two segments to pass parameters
-  get "actor_by_full_name_seg/:first_name/:last_name" => "actors#actor_by_full_name"
-  # Same scenario, but using post to pass first_name and last_name
-  post "actor_by_full_name" => "actors#actor_by_full_name"
+
+  # Restful Actor routes
+  get "/actors" => "actors#index"
+  post "/actors" => "actors#create"
+  get "/actors/:id" => "actors#show"
+  patch "/actors/:id" => "actors#update"
+  delete "/actors/:id" => "actors#destroy"
+
 end
