df157a8f3e7b675852463baaba48746d857abc18
vinnyk <ragdolleye@gmail.com>
2021-05-26 23:53:46 -0500

    Adds restful route and action for create for movie_genres. Tested add in insomnia.

--- app/models/movie_genre.rb
+++ app/models/movie_genre.rb
@@ -1,4 +1,6 @@
 class MovieGenre < ApplicationRecord
+  validates :movie_id, presence: true, numericality: { only_integer: true }
+  validates :genre_id, presence: true, numericality: { only_integer: true }
   belongs_to :genre
   belongs_to :movie
 end
